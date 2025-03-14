87b47c65385356d388d6307e507d6e246eeb316e
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2025-03-14 19:43:01 +0500

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,6 +20,11 @@
     @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
     @inertiaHead
 
+    <script
+			  src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+			  integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+			  crossorigin="anonymous"></script>
+
 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
 
 <style>
